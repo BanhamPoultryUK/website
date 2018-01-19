c85f6c700c6d6fc8910a3623e5d17122455aa20e
Lewis Goddard <lewisgoddard@eustasy.org>
2018-01-19 11:45:57 +0000

    ensure no www.

--- assets/sections/www-redirect.php
+++ assets/sections/www-redirect.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( $_SERVER['HTTP_HOST'] === 'www.banhampoultryuk.com' ) {
-	header('HTTP/1.1 301 Moved Permanently'); 
+	header('HTTP/1.1 301 Moved Permanently');
 	header('Location: http://banhampoultryuk.com'.$_SERVER['REQUEST_URI'].'?ref=www');
 	exit;
 }
