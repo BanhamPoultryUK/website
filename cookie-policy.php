b631b0855da0f1e0b05f5c07f9a4b78f42e094aa
Lewis Goddard <lewisgoddard@eustasy.org>
2018-05-18 15:02:07 +0100

    Wide content

--- cookie-policy.php
+++ cookie-policy.php
@@ -20,89 +20,87 @@
 				<a href="#profile">&#187; Product Profile</a>
 				-->
 			</div>
-			<div id="main_content">
-				<div class="content_div">
-					<h3 class="heading">Cookie Compliance</h3>
-					<table>
-						<tr>
-							<th>Cookie Name
-							<th>Type
-							<th>Party
-							<th>Persistence
-							<th>Expiration Time
-							<th>Personally Identifiable
-						</tr>
-						<tr>
-							<th colspan="6">Description
-						</tr>
-						<tr>
-							<td class="bold">cookie_compliance
-							<td>Preference
-							<td>First
-							<td>Permanent
-							<td>2038-01-19
-							<td>No
-						</tr>
-						<tr>
-							<td colspan="6">Marks that the user has accepted the Cookie Compliance banner.
-						</tr>
-						<tr>
-							<td class="bold">_ga
-							<td>Analytics
-							<td>Third
-							<td>Permanent
-							<td>2 years
-							<td>Yes
-						</tr>
-						<tr>
-							<td colspan="6">Used to distinguish users.
-						</tr>
-						<tr>
-							<td class="bold">_gid
-							<td>Analytics
-							<td>Third
-							<td>Temporary
-							<td>24 hours
-							<td>Yes
-						</tr>
-						<tr>
-							<td colspan="6">Used to distinguish users.
-						</tr>
-						<tr>
-							<td class="bold">_gat
-							<td>Analytics
-							<td>Third
-							<td>Temporary
-							<td>1 minute
-							<td>Yes
-						</tr>
-						<tr>
-							<td colspan="6">Used to throttle request rate.
-						</tr>
-						<tr>
-							<td class="bold">AMP_TOKEN
-							<td>Analytics
-							<td>Third
-							<td>Temporary / Permanent
-							<td>30 seconds to 1 year
-							<td>Yes
-						</tr>
-						<tr>
-							<td colspan="6">Contains a token that can be used to retrieve a Client ID from AMP Client ID service. Other possible values indicate opt-out, inflight request or an error retrieving a Client ID from AMP Client ID service.
-						</tr>
-						<tr>
-							<td class="bold">_gac_&lt;property-id&gt;
-							<td>Analytics
-							<td>Third
-							<td>Permanent
-							<td>90 days
-							<td>Yes
-						</tr>
-						<tr>
-							<td colspan="6">Contains campaign related information for the user. If you have linked your Google Analytics and AdWords accounts, AdWords website conversion tags will read this cookie unless you opt-out. <a href="https://support.google.com/adwords/answer/7521212" target="_blank">Learn more</a>.
-						</tr>
-					</table>
-				</div>
+			<div id="main_content" class="wide">
+				<h3 class="heading">Cookie Policy</h3>
+				<table>
+					<tr>
+						<th>Cookie Name
+						<th>Type
+						<th>Party
+						<th>Persistence
+						<th>Expiration Time
+						<th>Personally Identifiable
+					</tr>
+					<tr>
+						<th colspan="6">Description
+					</tr>
+					<tr>
+						<td class="bold">cookie_compliance
+						<td>Preference
+						<td>First
+						<td>Permanent
+						<td>2038-01-19
+						<td>No
+					</tr>
+					<tr>
+						<td colspan="6">Marks that the user has accepted the Cookie Compliance banner.
+					</tr>
+					<tr>
+						<td class="bold">_ga
+						<td>Analytics
+						<td>Third
+						<td>Permanent
+						<td>2 years
+						<td>Yes
+					</tr>
+					<tr>
+						<td colspan="6">Used to distinguish users.
+					</tr>
+					<tr>
+						<td class="bold">_gid
+						<td>Analytics
+						<td>Third
+						<td>Temporary
+						<td>24 hours
+						<td>Yes
+					</tr>
+					<tr>
+						<td colspan="6">Used to distinguish users.
+					</tr>
+					<tr>
+						<td class="bold">_gat
+						<td>Analytics
+						<td>Third
+						<td>Temporary
+						<td>1 minute
+						<td>Yes
+					</tr>
+					<tr>
+						<td colspan="6">Used to throttle request rate.
+					</tr>
+					<tr>
+						<td class="bold">AMP_TOKEN
+						<td>Analytics
+						<td>Third
+						<td>Temporary / Permanent
+						<td>30 seconds to 1 year
+						<td>Yes
+					</tr>
+					<tr>
+						<td colspan="6">Contains a token that can be used to retrieve a Client ID from AMP Client ID service. Other possible values indicate opt-out, inflight request or an error retrieving a Client ID from AMP Client ID service.
+					</tr>
+					<tr>
+						<td class="bold">_gac_&lt;property-id&gt;
+						<td>Analytics
+						<td>Third
+						<td>Permanent
+						<td>90 days
+						<td>Yes
+					</tr>
+					<tr>
+						<td colspan="6">Contains campaign related information for the user. If you have linked your Google Analytics and AdWords accounts, AdWords website conversion tags will read this cookie unless you opt-out. <a href="https://support.google.com/adwords/answer/7521212" target="_blank">Learn more</a>.
+					</tr>
+				</table>
 			</div>
 		</div>
 		
