e5341824ac66834ed1efd882d7182f028bdd7caf
Lewis Goddard <lewisgoddard@eustasy.org>
2018-11-12 12:11:40 +0000

    Add Modern Slavery Act Statement

--- assets/sections/footer.php
+++ assets/sections/footer.php
@@ -7,6 +7,7 @@
 					VAT Registration No. 307 0082 45</p>
 				<div class="ft_links">
 					<a href="/careers.php">Careers</a><br />
+					<a href="/assets/files/BanhamPoultryModernSlaveryActStatement.pdf">Modern Slavery Act</a><br />
 					<a href="https://mail.banhampoultryuk.com/owa/">Employee Mail</a>
 				</div>
 				<div class="ft_links">
