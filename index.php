049962a6981b3165cade596c3940d4ed05e5724e
Lewis Goddard <lewisgoddard@eustasy.org>
2019-08-24 12:23:20 +0100

    Add alt tags

--- index.php
+++ index.php
@@ -21,9 +21,9 @@
 		<div style="clear:both"></div>
 		
 		<div class="bottom-images">
-			<a href="agriculture_campylobacter.php"><img src="assets/images/act-fv2.jpg"></a>
-			<a href="http://www.greatbritishchicken.co.uk/welcome-to-love-chicken/"><img src="assets/images/lovechicken.jpg" style="max-width: 44%; height: auto;"></a>
-			<a href="http://www.ifr.ac.uk/news/latest-news/2014/06/dont-wash-raw-chicken-food-safety-week-2014/"><img src="assets/images/ifr-logo.jpg" style="float:right"></a>
+			<a href="agriculture_campylobacter.php"><img src="assets/images/act-fv2.jpg" alt="Acting on Camylobacter Together"></a>
+			<a href="http://www.greatbritishchicken.co.uk/welcome-to-love-chicken/"><img src="assets/images/lovechicken.jpg" style="max-width: 44%; height: auto;" alt="Love Chicken - Great British Chicken .co.uk"></a>
+			<a href="http://www.ifr.ac.uk/news/latest-news/2014/06/dont-wash-raw-chicken-food-safety-week-2014/"><img src="assets/images/ifr-logo.jpg" style="float:right" alt="IFR - Institute of Food Research"></a>
 		</div>
 		
 		
