91544427db6f87aef2eccfa500f5af45c7f750d8
Lewis Goddard <lewis.goddard@eustasy.org>
2022-06-30 14:46:39 +0100

    Update layout

--- careers.php
+++ careers.php
@@ -1,84 +1,83 @@
 <?php
-
 	$Title = 'Banham Poultry &middot; Careers';
 	$Description = 'Search for vacancies at Banham Poultry.';
 	$Keywords = 'banham poultry,banham group,fresh british chicken,poultry products,poultry processors,poultry farmers';
 	include 'assets/sections/header.php';
 ?>
-		<div id="main">
-		<div id="attach_form_div">
+
+<div id="main">
+	<div id="attach_form_div">
 		<form id='contactus' action="/_contact-form/send-mail_recruitment.php" method="post" enctype="multipart/form-data">
 			<fieldset >
 				<h3 class="heading">Contact us</h3>
 				<p>If you would like to send us your cv, please attach it to an application directed to <a href="mailto:hr@banhampoultryuk.com">hr@banhampoultryuk.com</a></p>
 				<br>
 				<br>
-			<div class='container'>
-				<h3><label for='name'>Name <span>*</span></label></h3>
-				<input type='text' name='name' id='name' value='' maxlength="128" required /><br/>
-			</div>
-			<div class='container'>
-				<h3><label for='email'>Email Address <span>*</span></label></h3>
-				<input type='email' name='email' id='email' value='' maxlength="128" required /><br/>
-			</div>
-			<div class='container'>
-				<h3><label for='message'>Message</label></h3>
-				<textarea rows="10" cols="50" name='message' id='message'></textarea>
-			</div>
-			<div class='container'>
-				<p>By submitting your details you consent to Banham Poultry (2018) Limited retaining them for legitimate business interests in line with current GDPR legislation.</p>
-				<br />
-				<input type='submit' id='submit' name='Submit' value='Send' />
-			</div>
-		</fieldset>
-	</form>
-		</div>
-			<div id="job_desc_div">
-				<h3 class="heading">Job Vacancies</h3>
-				<br>
-				<hr>
-				<br>
-				<h4>Multi-skilled Warehouse operative</h4>
-				<p>Banham Poultry LTD has exciting new position available – Multi Skilled Warehouse operative in food manufacturing plant with immediate start!</p>
-				<h4>Responsibilities:</h4>
-				<ul>
-					<li>Scanning, packing, loading, documenting packed goods for despatch
-					<li>Operating forklift to load and unload goods
-					<li>Keeping your workplace clean up to company standards
-					<li>Working according to health and safety standards
-				</ul>
-				<br>
-				<h4>Pay structure / rates:</h4>
-				<ul>
-					<li>Day shift 0600-1800 3 on / 1 off / 3 on / 7 off
-					<li>Night shift Monday to Friday 1800-0300 or 2100-0600
-					<li>Starting pay rate £10.30 p/h, after probation period - £10.61p/h.
-					<li>Overtimes available with increased rates!
-				</ul>
-				<br>
-				<h4>Requirements:</h4>
-					<ul>
-					<li>Forklift licence (preferred but not essential)
-					<li>Experience preferred but not essential as training will be given
-					<li>Own transport due to location of the factory
-				</ul>
-				<p>If you are interested please apply and we will contact you to arrange interview.</p>
-				<!--
-				<h3>We have no open positions at this time.</h3>
-				-->
-				<br>
-				<hr>
-				<br>
-				<h4>Discounted staff sales, life insurance, and on-site parking and canteen are available.</h4>
-				<br>
-				<h4>Please contact <a href="mailto:hr@banhampoultryuk.com">hr@banhampoultryuk.com</a> for further information.</h4>
-				<br>
-				<h4>We do not accept correspondence via Recruitment Agencies.</h4>
-				<br>
-				<p>If you have submitted a CV or Application Form you will hear from us, but please be patient.</p>
-				<p>Thank you for your interest.</p>
-			</div>
-		</div>
+				<div class='container'>
+					<h3><label for='name'>Name <span>*</span></label></h3>
+					<input type='text' name='name' id='name' value='' maxlength="128" required /><br/>
+				</div>
+				<div class='container'>
+					<h3><label for='email'>Email Address <span>*</span></label></h3>
+					<input type='email' name='email' id='email' value='' maxlength="128" required /><br/>
+				</div>
+				<div class='container'>
+					<h3><label for='message'>Message</label></h3>
+					<textarea rows="10" cols="50" name='message' id='message'></textarea>
+				</div>
+				<div class='container'>
+					<p>By submitting your details you consent to Banham Poultry (2018) Limited retaining them for legitimate business interests in line with current GDPR legislation.</p>
+					<br />
+					<input type='submit' id='submit' name='Submit' value='Send' />
+				</div>
+			</fieldset>
+		</form>
 	</div>
+	<div id="job_desc_div">
+		<h3 class="heading">Job Vacancies</h3>
+		<!--
+		<h3>We have no open positions at this time.</h3>
+		-->
+		<br>
+		<hr>
+		<br>
+		<h4>Multi-skilled Warehouse operative</h4>
+		<p>Banham Poultry LTD has exciting new position available – Multi Skilled Warehouse operative in food manufacturing plant with immediate start!</p>
+		<h4>Responsibilities:</h4>
+		<ul>
+			<li>Scanning, packing, loading, documenting packed goods for despatch
+			<li>Operating forklift to load and unload goods
+			<li>Keeping your workplace clean up to company standards
+			<li>Working according to health and safety standards
+		</ul>
+		<br>
+		<h4>Pay structure / rates:</h4>
+		<ul>
+			<li>Day shift 0600-1800 3 on / 1 off / 3 on / 7 off
+			<li>Night shift Monday to Friday 1800-0300 or 2100-0600
+			<li>Starting pay rate £10.30 p/h, after probation period - £10.61p/h.
+			<li>Overtimes available with increased rates!
+		</ul>
+		<br>
+		<h4>Requirements:</h4>
+			<ul>
+			<li>Forklift licence (preferred but not essential)
+			<li>Experience preferred but not essential as training will be given
+			<li>Own transport due to location of the factory
+		</ul>
+		<p>If you are interested please apply and we will contact you to arrange interview.</p>
+		<br>
+		<hr>
+		<br>
+		<h4>Discounted staff sales, life insurance, and on-site parking and canteen are available.</h4>
+		<br>
+		<h4>Please contact <a href="mailto:hr@banhampoultryuk.com">hr@banhampoultryuk.com</a> for further information.</h4>
+		<br>
+		<h4>We do not accept correspondence via Recruitment Agencies.</h4>
+		<br>
+		<p>If you have submitted a CV or Application Form you will hear from us, but please be patient.</p>
+		<p>Thank you for your interest.</p>
+	</div>
+</div>
 
-<?php include 'assets/sections/footer.php'; ?>
+<?php include 'assets/sections/footer.php';
--- contact_us.php
+++ contact_us.php
@@ -1,101 +1,90 @@
 <?php
-
 	$Title = 'Banham Poultry &middot; Contact Us';
 	$Description = 'Write to us, contact us, get directions to our site and request product literature.';
 	$Keywords = 'banham poultry,banham group,fresh british chicken,poultry products,poultry processors,poultry farmers';
 	include 'assets/sections/header.php';
-
 ?>
-		<div id="main">
-			<div id="contact_div">
 
-				<div class="detail">
-					<h3 class="heading">Write to us</h3>
-					<p>Banham Poultry (2018) Limited<br />Station Road<br />Attleborough<br />Norfolk<br />NR17 2AT<br />United Kingdom</p>
-				</div>
-
-				<div class="detail">
-					<h3 class="heading">Contact us</h3>
-					<p><span>t </span> +44 (0)1953 455001</p><br />
-					<p><span>f </span> +44 (0)1953 455466</p><br />
-<a id="email_link" href="mailto:hr@banhampoultryuk.com?subject=Enquiry%20via%20website">hr@banhampoultryuk.com</a><br />
-<a href="careers">For careers please click here</a>
-				</div>
-				<div class="detail">
-					<h3 class="heading">Find Us</h3>
-					<form id="direction" action="http://maps.google.com/maps" method="get">
-						<label for="direction_input">Source Address</label>
-						<input id="direction_input" type="text" onfocus="if(this.value=='Enter your postal code')this.value=''" onblur="if(this.value=='')this.value='Enter your postal code'" title="" value="Enter your postal code" name="saddr"/>
-						<input type="hidden" name="daddr" value="Banham Poultry, Station Road, Attleborough NR17 2AT, UK"/><br /><br />
-						<input id="direction_icon" type="image" src="assets/images/1340469161_Contacts.png" title="Get Directions" alt="Get Directions"/>
-					</form>
-				</div>
-
-			</div>
-			<div id="registration_div">
-
-				<h3 class="heading">Contact us</h3>
-				<p>Please use this form to make an enquiry or to request product literature</p><br/>
-				<p><span>&#42;</span> required fields</p><br/>
-
-
-
-				<form id="registration" action="/_contact-form/send-mail_enquiry.php" method="post">
-
-					<label for="suggestion" class="formlabel">Title</label>
-					<div class="formfield">
-						<select id="suggestion" name="suggestion">
-							<option selected="selected">Mr</option>
-							<option>Mrs</option>
-							<option>Ms</option>
-							<option>Miss</option>
-							<option>Dr</option>
-						</select>
-					</div>
-
-					<label for="name" class="formlabel">Name <span>&#42;</span></label>
-					<div class="formfield"><input class="field" type="text" id="name" name="name" size="45" required /></div>
-
-					<label for="company_name" class="formlabel">Company Name</label>
-					<div class="formfield"><input class="field" type="text" id="company_name" name="company_name" size="45" /></div>
-
-					<label for="job_title" class="formlabel">Job Title</label>
-					<div class="formfield"><input class="field" type="text" id="job_title" name="job_title" size="45" /></div>
-
-					<label for="email" class="formlabel">Email <span>&#42;</span></label>
-					<div class="formfield"><input class="field" type="email" id="email" name="email" size="45" required /></div>
-
-					<label for="address" class="formlabel">Address</label>
-					<div class="formfield">
-						<textarea id="address" name="address" rows="5" cols="40"></textarea>
-					</div>
-
-					<label for="tel_no" class="formlabel">Telephone</label>
-					<div class="formfield"><input class="field" type="text" id="tel_no" name="tel_no" size="45"/></div>
-
-					<label for="comments" class="formlabel">Nature of your enquiry <span>&#42;</span></label>
-					<div class="formfield">
-						<select name="enquiry_type">
-							<option value="hr" selected>HR</option>
-							<option value="sales">Sales</option>
-							<option value="other">Other</option>
-						</select>
-						<br />
-						<textarea id="comments" name="comments" rows="6" cols="40"></textarea>
-					</div>
-
-					<div id="button_div">
-						<input class="form_button" type="submit" value="Send" />
-						<input class="form_button" type="reset" value="Reset" />
-					</div>
-
-				</form>
-
-
-
-			</div>
-
+<div id="main">
+	<div id="contact_div">
+		<div class="detail">
+			<h3 class="heading">Write to us</h3>
+			<p>Banham Poultry (2018) Limited<br />Station Road<br />Attleborough<br />Norfolk<br />NR17 2AT<br />United Kingdom</p>
 		</div>
+
+		<div class="detail">
+			<h3 class="heading">Contact us</h3>
+			<p><span>t </span> +44 (0)1953 455001</p><br />
+			<p><span>f </span> +44 (0)1953 455466</p><br />
+			<a id="email_link" href="mailto:hr@banhampoultryuk.com?subject=Enquiry%20via%20website">hr@banhampoultryuk.com</a><br />
+			<a href="careers">For careers please click here</a>
+		</div>
+		<div class="detail">
+			<h3 class="heading">Find Us</h3>
+			<form id="direction" action="http://maps.google.com/maps" method="get">
+				<label for="direction_input">Source Address</label>
+				<input id="direction_input" type="text" onfocus="if(this.value=='Enter your postal code')this.value=''" onblur="if(this.value=='')this.value='Enter your postal code'" title="" value="Enter your postal code" name="saddr"/>
+				<input type="hidden" name="daddr" value="Banham Poultry, Station Road, Attleborough NR17 2AT, UK"/><br /><br />
+				<input id="direction_icon" type="image" src="assets/images/1340469161_Contacts.png" title="Get Directions" alt="Get Directions"/>
+			</form>
+		</div>
+
 	</div>
+	<div id="registration_div">
+
+		<h3 class="heading">Contact us</h3>
+		<p>Please use this form to make an enquiry or to request product literature</p><br/>
+		<p><span>&#42;</span> required fields</p><br/>
+
+		<form id="registration" action="/_contact-form/send-mail_enquiry.php" method="post">
+			<label for="suggestion" class="formlabel">Title</label>
+			<div class="formfield">
+				<select id="suggestion" name="suggestion">
+					<option selected="selected">Mr</option>
+					<option>Mrs</option>
+					<option>Ms</option>
+					<option>Miss</option>
+					<option>Dr</option>
+				</select>
+			</div>
+
+			<label for="name" class="formlabel">Name <span>&#42;</span></label>
+			<div class="formfield"><input class="field" type="text" id="name" name="name" size="45" required /></div>
+
+			<label for="company_name" class="formlabel">Company Name</label>
+			<div class="formfield"><input class="field" type="text" id="company_name" name="company_name" size="45" /></div>
+
+			<label for="job_title" class="formlabel">Job Title</label>
+			<div class="formfield"><input class="field" type="text" id="job_title" name="job_title" size="45" /></div>
+
+			<label for="email" class="formlabel">Email <span>&#42;</span></label>
+			<div class="formfield"><input class="field" type="email" id="email" name="email" size="45" required /></div>
+
+			<label for="address" class="formlabel">Address</label>
+			<div class="formfield">
+				<textarea id="address" name="address" rows="5" cols="40"></textarea>
+			</div>
+
+			<label for="tel_no" class="formlabel">Telephone</label>
+			<div class="formfield"><input class="field" type="text" id="tel_no" name="tel_no" size="45"/></div>
+
+			<label for="comments" class="formlabel">Nature of your enquiry <span>&#42;</span></label>
+			<div class="formfield">
+				<select name="enquiry_type">
+					<option value="hr" selected>HR</option>
+					<option value="sales">Sales</option>
+					<option value="other">Other</option>
+				</select>
+				<br />
+				<textarea id="comments" name="comments" rows="6" cols="40"></textarea>
+			</div>
+
+			<div id="button_div">
+				<input class="form_button" type="submit" value="Send" />
+				<input class="form_button" type="reset" value="Reset" />
+			</div>
+		</form>
+	</div>
+</div>
 
 <?php include 'assets/sections/footer.php';
